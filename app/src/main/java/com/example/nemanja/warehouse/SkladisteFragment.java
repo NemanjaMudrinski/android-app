daf499e4a33c9d4badddff835967d7d48d367674
NemanjaMudrinski <mudrinskinemanja@gmail.com>
2019-08-16 05:32:47 -0500

    Warehouse

---
+++ app/src/main/java/com/example/nemanja/warehouse/SkladisteFragment.java
@@ -0,0 +1,134 @@
+package com.example.nemanja.warehouse;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+
+public class SkladisteFragment extends Fragment implements View.OnClickListener  {
+
+    private OnFragmentInteractionListener mListener;
+
+    public SkladisteFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment.
+     * @return A new instance of fragment SkladistaFragment.
+     */
+    public static SkladisteFragment newInstance() {
+        SkladisteFragment fragment = new SkladisteFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @SuppressLint("HandlerLeak")
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View v = (View) inflater.inflate(R.layout.fragment_skladiste, container, false);
+        API.getJSON("http://10.0.2.2:5000/json", new ReadDataHandler(){
+            @Override
+            public void handleMessage(Message msg) {
+                String odgovor = getJson();
+
+                try {
+                    JSONArray array = new JSONArray(odgovor);
+                    //odgovor spakujemo u JSONArray, i onda parsiramo da bismo dobili linked list
+                    ArrayList<SkladisteModel> skladista = SkladisteModel.parseJSONArray(array);
+                    //postavljamo dobijeni povratni tekst kao tekst labele (ručno formatirano)
+
+                    LinearLayout listaSkladista = (LinearLayout) v.findViewById(R.id.listaSkladista);
+
+                    for (final SkladisteModel s : skladista) {
+
+                        LinearLayout item = (LinearLayout) inflater.inflate(R.layout.skladiste, null);
+
+                        TextView naziv = (TextView) item.findViewById(R.id.naziv);
+                        naziv.setKeyListener(null);
+                        naziv.setText(s.getNaziv());
+
+                        item.setTag(s.getId());
+
+                        item.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                mListener.odabranoJeSkladiste(s.getId());
+                            }
+                        });
+
+                        listaSkladista.addView(item);
+                    }
+
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+        return v;
+    }
+
+    public void onButtonPressed(Uri uri) {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            //throw new RuntimeException(context.toString()
+            //+ " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        String skladiste = (String) view.getTag();
+
+        if(mListener != null){
+            mListener.odabranoJeSkladiste(skladiste);
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void odabranoJeSkladiste(String skladiste);
+    }
+}
